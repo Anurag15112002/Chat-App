2eaf38210bd377fb9900f1e6ce7ab7ff2be28d97
Anurag Singh Rajput <anuragrajput8477@gmail.com>
2025-02-15 15:11:37 +0530

    final

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://chat-app-1-o1d5.onrender.com/api", // Replace with your Render URL
   withCredentials: true,
 });
