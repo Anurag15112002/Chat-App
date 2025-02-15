ebfedd0df8ba52cb54151f455ecb0962bb4dcf09
Anurag Singh Rajput <145913972+Anurag15112002@users.noreply.github.com>
2025-02-15 14:50:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-user-ijzr.onrender.com",
+    origin: "http://localhost:5175",
     credentials: true,
   })
 );
